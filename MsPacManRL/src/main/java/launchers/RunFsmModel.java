02f0ac11c026df9fdd4f9633ecd0ac2c399a0bd3
Dbugoi <60663710+Dbugoi@users.noreply.github.com>
2024-01-21 17:48:21 +0100

    Añadir Q-learning

---
+++ MsPacManRL/src/main/java/launchers/RunFsmModel.java
@@ -0,0 +1,25 @@
+package launchers;
+
+import ghosts.GhostAlgorithmic;
+import ghosts.GhostRandom;
+import mspacmans.Executor;
+import pacman.controllers.GhostController;
+
+/**
+ * Class that runs the game with a fsm 
+ */
+public class RunFsmModel {
+	public static void main(String[] args) {
+		//Class that executes the game
+		Executor executor = new Executor.Builder()
+				.setTickLimit(4000)
+				.setVisual(true)
+				.setScaleFactor(3.0)
+				.build();
+		
+		GhostController ghosts = new GhostAlgorithmic(); 	//The ghosts that we want to play against
+		
+
+		System.out.println(executor.runGameFSM(ghosts, 40)); //Runs and shows the game executing the FSM
+	} 
+}
